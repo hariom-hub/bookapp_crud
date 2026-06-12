339f70ef84990b48f8d054c9e3827cd925519a1f
harrybro <hariomthakur638@gmail.com>
2026-06-12 10:32:49 +0530

    crud is under development

--- src/services/book_service.py
+++ src/services/book_service.py
@@ -1,16 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.schemas import BookModel, CreateBookModel, UpdateBookModel
-from sqlalchemy import select
+from sqlalchemy import select, desc
 from src.model import Book
+from fastapi.exceptions import HTTPException
 
 
 class BookService:
 
     async def get_all_books(self, book: BookModel, session: AsyncSession):
-        pass
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.execute(statement)
+        return result.all()
 
     async def get_book_byId(self, book_id: int, session: AsyncSession):
-        pass
+        statement = select(Book).where(Book.id == book_id)
+        result = await  session.execute(statement)
+        book = result.first()
+        return book
 
     async def create_book(self, add_book: CreateBookModel, session: AsyncSession):
         pass
