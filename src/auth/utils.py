ef2d6a945461a9488ad3e75c44661c59f5284af0
harrybro <hariomthakur638@gmail.com>
2026-06-26 19:31:44 +0530

    pwdlib is now being used for password hashing with argon2 algorithm

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,13 +1,23 @@
-from passlib.context import CryptContext
-from passlib.handlers import bcrypt
+# from passlib.context import CryptContext
+from pwdlib import PasswordHash
+from datetime import timedelta, timezone
+import jwt
 
-password_context = CryptContext(
-    schemes = ['bcrypt']
-)
+
+
+password_hash = PasswordHash.recommended()
+
+# not using because passlib is being deprecated
+# password_context = CryptContext(
+#     schemes = ['bcrypt']
+# )
 
 def generate_password_hash(password:str)->str:
-    hashed_password = password_context.hash(password)
+    hashed_password = password_hash.hash(password)
     return hashed_password
 
 def verify_password_hash(password:str,hashed_password:str)->bool:
-    return password_context.verify(password,hashed_password)
+    return password_hash.verify(password,hashed_password)
+
+
+
--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     MYSQL_DB_URL : str
+    JWT_SECRET : str
+    JWT_ALGORITHM : str
     model_config = SettingsConfigDict(
         env_file = Path(__file__).parent.parent / ".env",
         extra = "ignore"
