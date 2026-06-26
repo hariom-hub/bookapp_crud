5a4b2e2d2510914262d24bd90ffb32bdae118aef
harrybro <hariomthakur638@gmail.com>
2026-06-26 22:50:26 +0530

    jwt added

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,23 +1,51 @@
-# from passlib.context import CryptContext
+# from passlib.context import CryptContext  # not using passlib bcoz it's outdated
+
+
 from pwdlib import PasswordHash
-from datetime import timedelta, timezone
+from datetime import timedelta, timezone, date, datetime
 import jwt
+import logging
 
-
+from src.config import Config
 
 password_hash = PasswordHash.recommended()
+
 
 # not using because passlib is being deprecated
 # password_context = CryptContext(
 #     schemes = ['bcrypt']
 # )
 
-def generate_password_hash(password:str)->str:
+def generate_password_hash(password: str) -> str:
     hashed_password = password_hash.hash(password)
     return hashed_password
 
-def verify_password_hash(password:str,hashed_password:str)->bool:
-    return password_hash.verify(password,hashed_password)
+
+def verify_password_hash(password: str, hashed_password: str) -> bool:
+    return password_hash.verify(password, hashed_password)
 
 
+def create_access_token(user_data: dict, expiry: timedelta = None):
+    payload = {}
 
+    payload['user'] = user_data
+    payload['exp'] = datetime.now() + expiry
+    token = jwt.encode(
+        payload=payload,
+        key=Config.JWT_SECRET,
+        algorithm=Config.JWT_ALGORITHM
+    )
+
+    return token
+
+
+def decode_token(token: str) -> dict:
+    try:
+        token_dict = jwt.decode(
+            token=token,
+            key=Config.JWT_SECRET,
+            algorithms=[Config.JWT_ALGORITHM]
+        )
+        return token_dict
+    except jwt.PyJWTError as e:
+        logging.exception(e)
--- src/config.py
+++ src/config.py
@@ -4,14 +4,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    MYSQL_DB_URL : str
-    JWT_SECRET : str
-    JWT_ALGORITHM : str
+    MYSQL_DB_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
     model_config = SettingsConfigDict(
-        env_file = Path(__file__).parent.parent / ".env",
-        extra = "ignore"
-    ) # attribute that will allow us to change the attributes of the pydantic model classes
+        env_file=Path(__file__).parent.parent / ".env",
+        extra="ignore"
+    )  # attribute that will allow us to change the attributes of the pydantic model classes
+
 
 Config = Settings()
-
-
